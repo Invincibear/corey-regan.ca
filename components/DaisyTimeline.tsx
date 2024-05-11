6c67ff574c25cf3e58ba667e525eaa609bdf168c
Corey Regan <regan.corey@gmail.com>
2024-05-10 22:37:20 -0700

    holy crap, I got framer-motion to work in nextjs 14!

--- components/DaisyTimeline.tsx
+++ components/DaisyTimeline.tsx
@@ -1,5 +1,9 @@
-import DaisyTimelineEvent from "@/components/DaisyTimelineEvent";
-import { TimelineEvent }  from "@/lib/TimelineEvents"
+"use client"
+
+import DaisyTimelineEvent          from "@/components/DaisyTimelineEvent"
+import { TimelineEvent }           from "@/lib/TimelineEvents"
+import { motion, AnimatePresence } from "framer-motion"
+
 
 import "@/styles/timeline.css"
 
@@ -12,13 +16,15 @@ export default function DaisyTimeline({ timelineEvents }: TimelineComponentProps
 
   return (
     <div id="timeline" className="flex justify-center items-center">
-      <ul className="pt-20 max-w-8xl timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
-        {timelineEvents.map(event => {
-          position = (position === "timeline-end") ? "timeline-start md:text-end" : "timeline-end"
+      <AnimatePresence mode="wait">
+        <motion.ul className="pt-20 max-w-8xl timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
+          {timelineEvents.map(timelineEvent => {
+            position = (position === "timeline-end") ? "timeline-start md:text-end" : "timeline-end"
 
-          return <DaisyTimelineEvent key={event.id} timelineEvent={event} position={position} />
-        })}
-      </ul>
+            return <DaisyTimelineEvent key={timelineEvent.id} timelineEvent={timelineEvent} position={position}/>
+          })}
+        </motion.ul>
+      </AnimatePresence>
     </div>
   )
 }
--- components/DaisyTimelineEvent.tsx
+++ components/DaisyTimelineEvent.tsx
@@ -1,16 +1,41 @@
+"use client"
+
 import Icon              from "@/components/Icon"
 import { TimelineEvent } from "@/lib/TimelineEvents"
 import Image             from "next/image"
 import Link              from "next/link"
+import { motion, AnimatePresence } from "framer-motion"
 
 
 interface DaisyTimelineEventProps {
   timelineEvent: TimelineEvent
   position:      string
 }
-export default function DaisyTimelineEvent({ timelineEvent, position = "start" }: DaisyTimelineEventProps) {
+export default function DaisyTimelineEvent({ timelineEvent, position = "timeline-start md:text-end" }: DaisyTimelineEventProps) {
+  const variants = {
+    hiddenFromLeft:  { opacity: 0, x: "-100%", y: 0 },
+    hiddenFromRight: { opacity: 0, x: " 100%", y: 0 },
+    enter:           { opacity: 1, x: 0,       y: 0 },
+    exitToLeft:      { opacity: 0, x: "-100%", y: 0 },
+    exitToRight:     { opacity: 0, x: " 100%", y: 0 },
+  }
+
+  const initial = (position === "timeline-end") ? "hiddenFromRight" : "hiddenFromLeft"
+  const whileInView = (position === "timeline-end") ? "timeline-start md:text-end" : "timeline-end"
+
   return (
-    <li>
+    <motion.li
+      key         = {timelineEvent.id}
+      initial     = {initial}
+      whileInView = "enter"
+      variants    = {variants}
+      viewport    = {{ once: true }}
+      className   = "overflow-hidden"
+      transition  = {{
+        duration: 0.5,
+        type:     "linear"
+      }}
+    >
       <div className="timeline-middle px-4 mb-2">
         {<Icon name={timelineEvent.icon}/>}
       </div>
@@ -31,11 +56,11 @@ export default function DaisyTimelineEvent({ timelineEvent, position = "start" }
           <div className={`flex justify-${position === 'timeline-end' ? 'start' : 'end'} my-2`}>
             <Link href={`/timeline/${timelineEvent.logo.src}`}>
               <Image
-                src     = {`/timeline/${timelineEvent.logo.src}`}
-                alt     = {timelineEvent.logo.alt ?? ""}
-                width   = {timelineEvent.logo.width}
-                height  = {timelineEvent.logo.height}
-                loading = "lazy"
+                src={`/timeline/${timelineEvent.logo.src}`}
+                alt={timelineEvent.logo.alt ?? ""}
+                width={timelineEvent.logo.width}
+                height={timelineEvent.logo.height}
+                loading="lazy"
               />
             </Link>
           </div>
@@ -47,11 +72,11 @@ export default function DaisyTimelineEvent({ timelineEvent, position = "start" }
           <div className={`flex justify-${position === 'timeline-end' ? 'start' : 'end'} mt-4`}>
             <Link href={`/timeline/${timelineEvent.image.src}`}>
               <Image
-                src     = {`/timeline/${timelineEvent.image.src}`}
-                alt     = {timelineEvent.image.alt ?? ""}
-                width   = {timelineEvent.image.width}
-                height  = {timelineEvent.image.height}
-                loading = "lazy"
+                src={`/timeline/${timelineEvent.image.src}`}
+                alt={timelineEvent.image.alt ?? ""}
+                width={timelineEvent.image.width}
+                height={timelineEvent.image.height}
+                loading="lazy"
               />
             </Link>
           </div>
@@ -73,6 +98,6 @@ export default function DaisyTimelineEvent({ timelineEvent, position = "start" }
         )}
       </div>
       <hr/>
-    </li>
+    </motion.li>
   )
 }
