8b69a1f5c780c921f63c009b088382c59793d2ef
Corey Regan <regan.corey@gmail.com>
2024-05-11 22:29:19 -0700

    formatting

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`h-full overflow-x-hidden bg-background text-default text-base ${inter.className}`} data-theme="dark">
+      <body
+        className={`h-full overflow-x-hidden bg-background text-default text-base ${inter.className}`}
+        data-theme="dark"
+      >
         <ThemeProviders>
           {children}
           <Footer />
