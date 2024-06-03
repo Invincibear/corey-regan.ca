742eeee2332e44b96757793cf1e600e326b37037
Corey Regan <regan.corey@gmail.com>
2024-06-02 18:16:37 -0700

    starfield minor improvements

--- components/StarfieldHero.tsx
+++ components/StarfieldHero.tsx
@@ -1,17 +1,21 @@
 "use client"
 
-import Starfield             from "@/components/Starfield"
-import { motion }            from "framer-motion"
-import { useRef, useEffect } from "react"
-import Typed                 from "typed.js"
+import Starfield from "@/components/Starfield"
+import Typed     from "typed.js"
 
-import "@/styles/starfield.css"
+import { motion, useMotionValueEvent, useScroll, useSpring, useTransform } from "framer-motion"
+import { useEffect, useRef, useState } from "react"
+
 import "@/styles/shimmer.css"
+import "@/styles/starfield.css"
 
 
 export default function StarfieldHero() {
+  const divRef        = useRef(null)
+  const sectionRef    = useRef(null)
   const typedWordsRef = useRef(null)
 
+  // Initialize Typed.js animated typing
   useEffect(() => {
     const typed = new Typed(typedWordsRef.current, {
       strings:       ['Site Reliability', 'DevOps', 'Full-Stack', 'Azure Cloud', 'AWS Cloud'],
@@ -26,44 +30,100 @@ export default function StarfieldHero() {
       contentType:   'html',
     })
 
+    // Destroy Typed instance during component unmount to stop animation
+    // This also prevents duplicate '|' cursor
     return () => {
-      // Destroy Typed instance during component unmount to stop animation
-      // This also prevents duplicate '|' cursor
-      typed.destroy();
+      typed.destroy()
     }
-  }, []);
+  }, [])
+
+  const sectionVariants = {
+    visible: {
+      // opacity: 1,
+      scale: 1,
+      transition: {
+        duration: .69 // This controls how fast the section comes into normal view
+      }
+    },
+    hidden: {
+      // opacity: 0,
+      scale:   0,
+      transition: {
+        duration: 2 // Duration of the animation when the section goes out of view
+      }
+    }
+  }
+
+  const { scrollYProgress } = useScroll({
+    // target: divRef,
+  })
+  const transformedOpacity = useTransform(
+    scrollYProgress,
+    [0, .069],
+    [1, 0] // Reduce opacity the further down user scrolls
+  )
+  const transformedScale = useTransform(
+    scrollYProgress,
+    [0, .069],
+    [1, 2] // Increase scale the further down user scrolls
+  )
 
   return (
-    <section id="hero" className="relative w-full h-screen bg-black">
-      <Starfield />
-      <div className="relative w-full h-screen flex items-center justify-center overflow-x-hidden">
-        <div
-          id        = "hero-title"
-          className = "flex flex-col gap-2 self-auto justify-self-end text-center sm:gap-4"
-          style     = {{ transform: "translateZ(0)" }}
-        >
-          <motion.h1
-            initial     = {{ opacity: 0 }}
-            whileInView = {{ opacity: 1 }}
-            transition  = {{ duration: 5 }}
-            className   = "hero-heading-rich-text text-center font-bold tracking-tighter text-8xl pb-4"
+    <motion.section
+      className = "w-full h-screen bg-black overflow-hidden"
+      ref       = {sectionRef}
+      style       = {{
+        opacity: transformedOpacity,
+        scale:   transformedScale,
+        // scale:   useSpring(transformedScale, { bounce: 0 }),
+      }}
+    >
+      <motion.div
+        className   = "w-full h-full"
+        id          = "hero"
+        // initial     = "hidden"
+        // whileInView = "visible"
+        // exit        = "hidden"
+        ref         = {divRef}
+        // variants    = {sectionVariants}
+        style       = {{
+          position: "relative",
+          opacity: transformedOpacity,
+          scale:   transformedScale,
+          // scale:   useSpring(transformedScale, { bounce: 0 }),
+        }}
+      >
+        <Starfield />
+        <div className="w-full h-screen flex items-center justify-center overflow-x-hidden">
+          <div
+            id        = "hero-title"
+            className = "flex flex-col gap-2 self-auto justify-self-end text-center sm:gap-4"
+            style     = {{ transform: "translateZ(0)" }}
           >
-            Corey
-            <br/>
-            Regan
-          </motion.h1>
-          <h2 className="hero-heading-rich-text w-richtext font-bold tracking-tight text-secondary text-2xl sm:text-5xl lg:text-6xl">
-            <div className="flex justify-center items-baseline">
-              <div className="w-120 text-right pr-3">
-                <strong ref={typedWordsRef} className="typed-words font-bold"></strong>
+            <motion.h1
+              className   = "hero-heading-rich-text text-center font-bold tracking-tighter text-8xl pb-4"
+              initial     = {{ opacity: 0, scale: 0 }}
+              transition  = {{ duration: 3 }}
+              viewport    = {{ once: true }}
+              whileInView = {{ opacity: 1, scale: 1 }}
+            >
+              Corey
+              <br/>
+              Regan
+            </motion.h1>
+            <h2 className="hero-heading-rich-text w-richtext font-bold tracking-tight text-secondary text-2xl sm:text-5xl lg:text-6xl">
+              <div className="flex justify-center items-baseline">
+                <div className="w-120 text-right pr-3">
+                  <strong ref={typedWordsRef} className="typed-words font-bold"></strong>
+                </div>
+                <div className="w-120 text-left pl-3">
+                  <span className="font-bold shimmer">Engineer</span>
+                </div>
               </div>
-              <div className="w-120 text-left pl-3">
-                <span className="font-bold shimmer">Engineer</span>
-              </div>
-            </div>
-          </h2>
+            </h2>
+          </div>
         </div>
-      </div>
-    </section>
+      </motion.div>
+    </motion.section>
   )
 }
