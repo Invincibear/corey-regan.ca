820365fd19afb34aa31aeadb840f97f237cc90fc
Corey Regan <regan.corey@gmail.com>
2024-06-11 12:11:57 -0700

    Customized dynamic OpenGraph image (WIP)

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -1,5 +1,6 @@
+import { BlogConfig }    from "@/config/blog"
 import { SocialLinks }   from "@/config/links"
-import { SiteConfig }    from "@/config/site"
+import { DOMAIN }        from "@/config/site"
 import { ImageResponse } from "next/og"
 import { NextRequest }   from "next/server"
 
@@ -25,22 +26,32 @@ export async function GET(req: NextRequest) {
 
     return new ImageResponse(
       (
-        <div tw="flex relative flex-col p-12 w-full h-full items-start text-black bg-white">
+        <div
+          tw="flex relative flex-col p-12 w-full h-full items-start text-black bg-white"
+          style={{
+            // backgroundImage: "http://localhost/opengraph-image.png",
+          }}
+        >
           <div tw="flex items-center">
             <svg
-              xmlns          = "http://www.w3.org/2000/svg"
-              viewBox        = "0 0 24 24"
-              fill           = "none"
-              stroke         = "currentColor"
-              strokeWidth    = "2"
-              strokeLinecap  = "round"
-              strokeLinejoin = "round"
-            >
-              <path d="M4 11a9 9 0 0 1 9 9" />
-              <path d="M4 4a16 16 0 0 1 16 16" />
-              <circle cx="5" cy="19" r="1" />
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="lucide lucide-sparkles">
+              <path
+                d="M9.937 15.5A2 2 0 0 0 8.5 14.063l-6.135-1.582a.5.5 0 0 1 0-.962L8.5 9.936A2 2 0 0 0 9.937 8.5l1.582-6.135a.5.5 0 0 1 .963 0L14.063 8.5A2 2 0 0 0 15.5 9.937l6.135 1.581a.5.5 0 0 1 0 .964L15.5 14.063a2 2 0 0 0-1.437 1.437l-1.582 6.135a.5.5 0 0 1-.963 0z"/>
+              <path d="M20 3v4"/>
+              <path d="M22 5h-4"/>
+              <path d="M4 17v2"/>
+              <path d="M5 18H3"/>
             </svg>
-            <p tw="ml-2 font-bold text-2xl">JollyBlog</p>
+            <p tw="ml-2 font-bold text-2xl">{BlogConfig.name}</p>
           </div>
           <div tw="flex flex-col flex-1 py-10">
             <div tw="flex text-xl uppercase font-bold tracking-tight font-normal">
@@ -49,7 +60,7 @@ export async function GET(req: NextRequest) {
             <div tw="flex text-[80px] font-bold text-[50px]">{heading}</div>
           </div>
           <div tw="flex items-center w-full justify-between">
-            <div tw="flex text-xl">{SiteConfig.url}</div>
+            <div tw="flex text-xl">{DOMAIN}</div>
             <div tw="flex items-center text-xl">
               <div tw="flex ml-2">{SocialLinks.github}</div>
             </div>
@@ -57,8 +68,8 @@ export async function GET(req: NextRequest) {
         </div>
       ),
       {
-        width: 1200,
-        height: 630,
+        width: 1600,
+        height: 842,
         fonts: [
           {
             name: "Inter",
